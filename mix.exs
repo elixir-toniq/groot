7772ba6bb2288188d0ea0336967477d11354b953
Chris Keathley <c@keathley.io>
2020-01-05 13:50:53 -0500

    Prep for publishing to hex.

--- mix.exs
+++ mix.exs
@@ -1,14 +1,22 @@
 defmodule Groot.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :groot,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+
+      description: description(),
+      package: package(),
+      name: "Groot",
+      source_url: "https://github.com/keathley/groot",
+      docs: docs(),
     ]
   end
 
@@ -25,13 +33,37 @@ defmodule Groot.MixProject do
     [
       {:hlclock, "~> 1.0"},
       {:local_cluster, "~> 1.0", only: [:dev, :test]},
-      {:schism, "~> 1.0", only: [:dev, :test]}
+      {:schism, "~> 1.0", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
 
   def aliases do
     [
       test: ["test --no-start"]
+    ]
+  end
+
+  def description do
+    """
+    Groot is a distributed KV store built on distributed erlang, LWW Register
+    CRDTS, and Hybrid Logical Clocks.
+    """
+  end
+
+  def package do
+    [
+      name: "groot",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/keathley/groot"},
+    ]
+  end
+
+  def docs do
+    [
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/keathley/groot",
+      main: "Groot",
     ]
   end
 end
